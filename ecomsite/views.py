025752fe4070e44a782e87d3f9ff7ea33c8b28ea
ben199712 <orijibenjamin1997@gmail.com>
2025-06-11 04:19:28 -0700

    vintage first commit

---
+++ ecomsite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from store.models import Product
+
+def home(request):
+    coming_soon_products = Product.objects.filter(is_available=True, coming_soon=True).order_by('-modified_date')[:12]
+    products = Product.objects.filter(is_available=True).order_by('-modified_date')[:12]
+    
+
+    context = {
+        'products': products,
+        'coming_soon_products': coming_soon_products,
+    }
+    return render(request, 'index.html', context)
